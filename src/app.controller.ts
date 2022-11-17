49ab912c8e599db78128cddc7971b75bc7d11c31
sjl <906320505@qq.com>
2022-11-17 20:52:41 +0800

    拦截器

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Req,
+  Request
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +15,15 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Param() params): string {
     return this.appService.getHello();
+  }
+
+  @Post('/postFn')
+  @HttpCode(201) // 修改状态码
+  @Header('Cache-Control', 'none') // 自定义响应头
+  postFn(@Req() req: Request): string {
+    console.log(req);
+    return 'postFn';
   }
 }
---
+++ src/user-module/user-module.module.ts
@@ -0,0 +1,33 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { LoggerMiddlewareModule } from 'src/middle-ware/logger-middleware/logger-middleware.module';
+import { LoggerMiddleware } from 'src/middle-ware/logger-middleware/logger.middleware';
+import { UserController } from './controller/user.controller';
+
+@Module({
+    imports:[LoggerMiddlewareModule],
+    controllers:[UserController]
+})
+
+/** 模块使用中间件 必须满足NestModule接口 */
+export class UserModuleModule implements NestModule{
+
+    // 1. 类中间件
+    configure(consumer:MiddlewareConsumer){
+      consumer.apply(LoggerMiddleware).forRoutes('user') 
+    } 
+
+
+    // 2.函数中间件
+    // configure(consumer:MiddlewareConsumer){
+    //     consumer.apply(logger).forRoutes('user') 
+    // } 
+
+
+    // 3.顺序执行的中间件
+    // configure(consumer:MiddlewareConsumer){
+    //     consumer.apply(cors(),helmet(),logger).forRoutes('user') 
+    // } 
+
+
+
+  }
