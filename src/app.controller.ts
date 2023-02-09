e199626742bbb41bf49910245dfc2022070ed86a
sjl <906320505@qq.com>
2023-02-09 12:05:28 +0800

    Jwt 登录验证

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,30 +12,39 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 import { logger } from './common/middleware/logger-middleware/logger.middleware';
+import { AuthService } from './module/auth/auth.service';
+import { Public } from './module/auth/constant/constant';
+import { JwtAuthGuard } from './module/auth/guard/jwt-auth.guard';
 import { LocalAuthGuard } from './module/auth/guard/loacl-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService , private authService:AuthService) {}
 
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req){
-    return req.user
+    return this.authService.login(req.body)
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.body;
+  }
 
+  @Public()
   @Get()
-  getHello(@Param() params): string {
-    return this.appService.getHello();
+  findAll() {
+    return [];
   }
+
 
   @Post('/postFn')
   @HttpCode(201) // 修改状态码
   @Header('Cache-Control', 'none') // 自定义响应头
   postFn(@Req() req: Request): string {
-    console.log(req);
     return 'postFn';
   }
 }
--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserModuleModule } from '../user-module/user-module.module';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constant/constant';
 import { LocalAuthGuard } from './guard/loacl-auth.guard';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-    imports:[UserModuleModule , PassportModule],
-    exports:[LocalAuthGuard],
-    providers:[AuthService , LocalStrategy],
+    imports:[UserModuleModule , PassportModule ,
+    JwtModule.register({
+        secret:jwtConstants.secret,
+        signOptions:{ expiresIn:`${jwtConstants.expiresIn}m`}
+    })
+],
+    exports:[LocalAuthGuard,AuthService],
+    providers:[AuthService , LocalStrategy , LocalAuthGuard , JwtStrategy],
     controllers:[]
 })
 export class AuthModule {}
