59a7bb1025158208887e7f7a47bf1d00ba498a8f
sjl <906320505@qq.com>
2023-02-10 11:23:33 +0800

    修饰器

--- src/common/decorator/roles.decorator.ts
+++ src/common/decorator/roles.decorator.ts
@@ -1,6 +1,10 @@
 import { SetMetadata } from "@nestjs/common";
+import { MyLogger } from "src/module/logger/logger.service";
 
 export const Roles = (...roles:string[]) => SetMetadata('roles',roles)
 
 const a = Roles('2')
 console.log(a);
+
+
+
